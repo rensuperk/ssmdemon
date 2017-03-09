2e4729f071d55fee6896e1d93845772dddc018ab
renkai <304078213@qq.com>
2017-03-09 15:35:33 +0800

    bug

--- demonAction/src/main/java/com/ssmdemon/rk/action/BookAction.java
+++ demonAction/src/main/java/com/ssmdemon/rk/action/BookAction.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -15,32 +16,39 @@ import java.util.List;
  * Created by renkai on 2017/3/9.
  */
 @Controller
-@RequestMapping("book")
+@RequestMapping("/book")
 public class BookAction {
     @Autowired
     private BookService bookService;
 
+    @ResponseBody
+    @RequestMapping("save")
     public RespDto save(Book book) {
         bookService.save(book);
         return new RespDto();
     }
     @RequestMapping("list")
+    @ResponseBody
     public RespDto list(Book book, @RequestParam(defaultValue = "0") Integer offset,@RequestParam(defaultValue = "10") Integer limit) {
         List<Book> list = bookService.list(book,offset,limit);
         Long count = bookService.count(book);
         return new RespDto().put("rows",list).put("count",count);
     }
     @RequestMapping("{id}")
+    @ResponseBody
     public RespDto get(@PathVariable(value = "id") Long id) {
         Book book  = bookService.get(id);
         return new RespDto().put("obj",book);
     }
+
     @RequestMapping("update")
+    @ResponseBody
     public RespDto Update(Book book) {
         bookService.update(book);
         return new RespDto();
     }
     @RequestMapping("delete/{id}")
+    @ResponseBody
     public RespDto delete( @PathVariable(value = "id") Long id) {
 
         bookService.delete(id);
